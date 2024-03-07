b57e359c76a1e9981a016e10d122db25e1042618
esraelif <elifesratunca@gmail.com>
2024-03-07 13:32:10 +0100

    HomeAdded

--- src/components/AddPatient.jsx
+++ src/components/AddPatient.jsx
@@ -1,25 +1,57 @@
-import React, { useState } from 'react';
-import '../App.css'
+import React, { useState } from "react";
+import uuid from "react-uuid"
+const AddPatient = ({ hastalar, setHastalar, doctors }) => {
+    const [hastaName, setHstName] = useState("");
+    const [date, setDate] = useState("");
 
-const AddPatient = ({ hastalar, setHastalar }) => {
-    const [hastaName, setHstName] = useState("")
-    const [date, setDate] = useState("")
+    const handleSubmit = (e) => {
+        e.preventDefault();
+
+        setHastalar([
+            ...hastalar,
+            {
+                id: uuid(),
+                patientName: hastaName,
+                day: date,
+                isDone: false,
+                myDoctor: doctors[0].doctorName,
+            },
+        ]);
+        setHstName("");
+        setDate("");
+        // submit sonrası inputlardan value temizlemek için, hem alttaki işlemler yapılır, hemde inputlarda value={hastaName} yazarak browser da boşluksa boşluk isimse isim gözükmesi sağlanır
+    };
+    console.log(hastalar);
     return (
         <div>
-            <form>
-                <div className='form-control'>
-                    <label htmlFor="name">Patient Information</label>
-                    <input id='name' type="text" onChange={(e) => setHstName(e.target.value)} />
+            <form onSubmit={handleSubmit}>
+                <div className="form-control">
+                    <label htmlFor="name"> Patient Information</label>
+                    <input
+                        id="name"
+                        type="text"
+                        onChange={(e) => setHstName(e.target.value)}
+                        value={hastaName}
+                    />
                 </div>
-                <div className='form-control'>
-                    <label htmlFor="">Day & Time</label>
-                    <input type="datetime" onChange={(e) => setDate(e.target.value)} />
-                </div>
-                <button type='submit' className='doc'><span>{hastalar.name} </span> için kayıt oluştur</button>
-            </form>
 
+                <div className="form-control">
+                    <label htmlFor=""> Day & Time</label>
+                    <input
+                        type="datetime-local"
+                        onChange={(e) => setDate(e.target.value)}
+                        value={date}
+                    />
+                </div>
+
+                <div>
+                    <button className="doc" type="submit">
+                        <span>{doctors[0].doctorName} </span> için kayıt oluştur
+                    </button>
+                </div>
+            </form>
         </div>
     );
-}
+};
 
 export default AddPatient;
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,35 +1,82 @@
-import React, { useState } from 'react';
-import { hastaData, doctorData } from '../helper/Data';
-import PatientList from '../components/PatientList';
-import '../App.css';
-import AddPatient from '../components/AddPatient';
-
+import React, { useState } from "react";
+import { hastaData, doctorData } from "../helper/Data";
+import PatientList from "../components/PatientList";
+import AddPatient from "../components/AddPatient";
 
 const Home = () => {
-    const [doctors, setDoctors] = useState(doctorData)
-    const [hastalar, setHastalar] = useState(hastaData)
+    let [doctors, setDoctors] = useState(doctorData);
+    const [hastalar, setHastalar] = useState(hastaData);
+    const [show, setShow] = useState(true);
+
+    const doctorClick = (idD) => {
+
+        //!1.yol
+        if (doctors.length === 4) {
+            setShow(false);
+            doctors = doctors.filter((a) => a.id === idD);
+            setDoctors(doctors);
+
+            setHastalar(hastalar.filter((a) => a.myDoctor === doctors[0].doctorName));
+        } else {
+            setShow(true);
+
+            setDoctors(doctorData);
+            setHastalar(hastaData);
+        }
+
+        //!2.yol 
+        // setShow(!show);
+
+        // //doktorlar dizisini tıklanan doktorla güncelle
+        // setDoctors(show ? doctors.filter((i) => i.id === idD) : doctorData);
+    };
+
     return (
-        <div>
+        <div style={{ display: show ? "block" : "flex" }}>
             <div>
                 <header>
                     <h1>HOSPITAL</h1>
-                    <div>
+                    <div className="dr">
                         {doctors.map((dr) => (
-                            <div className='dr' key={dr.id}>
-                                <img src={dr.doctorImg} className='btn' style={{ background: "aqua" }} width="180px" height="150px" alt="" />
-                                <h4 style={{ background: "aqua", borderLeft: "10px solid blue" }}>{dr.doctorName}</h4>
+                            <div key={dr.id}>
+                                <img
+                                    src={dr.doctorImg}
+                                    width="180px"
+                                    height="150px"
+                                    className="btn"
+                                    style={{ background: show ? "aqua" : "lightgreen" }}
+                                    alt=""
+                                    onClick={() => doctorClick(dr.id)}
+                                />
+                                <h4
+                                    style={{
+                                        background: show ? "aqua" : "lightgreen",
+                                        borderLeft: `10px solid ${show ? "blue" : "green"}`,
+                                    }}
+                                >
+                                    {dr.doctorName}{" "}
+                                </h4>
                             </div>
                         ))}
-
                     </div>
                 </header>
 
-                <AddPatient hastalar={hastalar} setHastalar={setHastalar} />
+                {!show && (
+                    <AddPatient
+                        hastalar={hastalar}
+                        setHastalar={setHastalar}
+                        doctors={doctors}
+                    />
+                )}
             </div>
 
-            <PatientList hastalar={hastalar} setHastalar={setHastalar} />
+            <PatientList
+                hastalar={hastalar}
+                setHastalar={setHastalar}
+                doctors={doctors}
+            />
         </div>
     );
-}
+};
 
 export default Home;
