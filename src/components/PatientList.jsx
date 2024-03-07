ec7b1198fc967a2253d2b37c1325f4c106d55a4e
esraelif <elifesratunca@gmail.com>
2024-03-07 10:28:35 +0100

    HomeAdded

--- src/components/PatientList.jsx
+++ src/components/PatientList.jsx
@@ -1,9 +1,23 @@
 import React from 'react';
+import { FaTimesCircle } from "react-icons/fa";
 
-const PatientList = () => {
+const PatientList = ({ hastalar, setHastalar }) => {
     return (
         <div>
+            {hastalar.map((patient) => (
+                <div className={patient.isDone ? "trueStill" : "falseStyle"}
+                    onDoubleClick={() => setHastalar(hastalar.map((hst) => hst.id === patient.id ? { ...patient, isDone: !hst.isDone } : hst))}>
+                    <div>
+                        <h2>{patient.patientName}</h2>
+                        <h4>{patient.day}</h4>
+                        <h3>{patient.myDoctor}</h3>
+                    </div>
 
+
+                    <FaTimesCircle onClick={ } style={{ color: "red" }} />
+                </div>
+
+            ))}
         </div>
     );
 }
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,9 +1,32 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { hastaData, doctorData } from '../helper/Data';
+import PatientList from '../components/PatientList';
+import '../App';
+
 
 const Home = () => {
+    const [doctors, setDoctors] = useState(doctorData)
+    const [hastalar, setHastalar] = useState(hastaData)
     return (
         <div>
+            <div>
+                <header>
+                    <h1>HOSPITAL</h1>
+                    <div>
+                        {doctors.map((dr) => (
+                            <div className='dr' key={dr.id}>
+                                <img src={dr.doctorImg} className='btn' style={{ background: "aqua" }} width="180px" height="150px" alt="" />
+                                <h4 style={{ background: "aqua", borderLeft: "10px solid blue" }}>{dr.doctorName}</h4>
+                            </div>
+                        ))}
 
+                    </div>
+                </header>
+
+                {/* hastaekle */}
+            </div>
+
+            <PatientList hastalar={hastalar} setHastalar={setHastalar} />
         </div>
     );
 }
